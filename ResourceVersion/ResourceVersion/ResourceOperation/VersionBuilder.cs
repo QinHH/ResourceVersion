a5e9eec391f353087b6a6567d986938e865bad00
LukeQin <Luke_0629@hotmail.com>
2015-09-10 22:35:45 +0800

    Add versionbuilder Del LZ4

---
+++ ResourceVersion/ResourceVersion/ResourceOperation/VersionBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceVersion.ResourceOperation
+{
+    public class VersionBuilder
+    {
+        static public void BuildVersion()
+        {
+            DeleteOldResource();
+
+            
+        }
+
+        static void DeleteOldResource()
+        {
+            if(Directory.Exists(VersionManager.strCueOutPath))
+            {
+                Directory.Delete(VersionManager.strCueOutPath);
+            }
+
+            Directory.CreateDirectory(VersionManager.strCueOutPath);
+        }
+    }
+}
